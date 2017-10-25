9aaa1827b135a8106b2458c0476eb9f5a94eb695
unknown <nikoraposo@gmail.com>
2017-10-25 15:51:20 -0300

    Adding a new class to modelate the Hero entity Adding a ngrepeater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+    </li>
+  </ul>
+`
 })
 export class AppComponent  { 
   title : string; 
-  hero : string;
-  heroes : string [];
+  myHero : string;
+  heroes : Hero [];
 
   constructor(){
     this.title = "Tour of heroes";
-    this.heroes = ["Superman", "Batman", "Robin", "Chapulin Colorado"]
-    this.hero = this.heroes[0];
-    
+    this.heroes = [
+      new Hero(1, "Batman"),
+      new Hero(2, "Superman"),
+      new Hero(3, "Wonder Woman")
+    ];
+    this.myHero = this.heroes[0].name;
   }
 }
